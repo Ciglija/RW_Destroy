cad14dbde21ca72b08497f4c7f6b4f8fdd780bf3
Ilija <kasicilija12@gmail.com>
2025-03-28 13:07:12 +0100

    Cleared branch

--- excel_importer.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-from sqlalchemy import create_engine
-
-
-def import_excel_to_db(file_path, table_name, db_engine):
-    df = pd.read_excel(file_path)
-    df.to_sql(table_name, con=db_engine, if_exists='replace', index=False)
